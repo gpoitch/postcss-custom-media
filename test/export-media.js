c8186a9f8a7ab94e7f5ec962aa816b7b93dc16cf
Garth Poitras <garth22@gmail.com>
2018-09-21 09:00:43 -0400

    failing test

--- test/export-media.js
+++ test/export-media.js
@@ -2,6 +2,7 @@ module.exports = {
 	customMedia: {
 		'--mq-a': '(max-width: 30em), (max-height: 30em)',
 		'--not-mq-a': 'not all and (--mq-a)',
+		'--mq-b': 'screen and (max-width: 30em)',
 		'--circular-mq-a': '(--circular-mq-b)',
 		'--circular-mq-b': '(--circular-mq-a)'
 	}
